b51aab98118be70a0e61fbaa7cdd0fa4a6a146c8
CallumGame <callumutd@live.co.uk>
2024-01-16 17:24:09 +0300

    created random number gen and array of colors

--- game.js
+++ game.js
@@ -1,1 +1,9 @@
-alert("hello")
+// alert("hello")
+var buttonColors = ["red", "blue", "green", "blue"];
+var gamePattern = [];
+
+function nextSequence(){
+    var randomNumber = Math.random();
+    randomNumber = randomNumber * 4;
+    randomNumber = Math.floor(randomNumber);
+};
