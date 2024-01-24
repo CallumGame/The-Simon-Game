1f7f3e4cc78e24084447ebbeb3874ff8db9ec139
CallumGame <callumutd@live.co.uk>
2024-01-24 08:41:47 +0300

    added a class that highlights the button pressed

--- game.js
+++ game.js
@@ -1,38 +1,38 @@
 var buttonColors = ["red", "blue", "green", "blue"];
 var gamePattern = [];
-var userClickedPattern = []
+var userClickedPattern = [];
 
+// Bind the click event outside the function
+$(".btn").click(function () {
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+});
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * buttonColors.length);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
 
-    var audio = new Audio("sounds/" + gamePattern + ".mp3");
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
     audio.play();
-    
 
-    $("#" + gamePattern[0]).fadeOut(100).fadeIn(100);
-
-   $(".btn").click(function(){
-    var userChosenColor = $(this).attr("id");
-    userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern);
-    playSound(userChosenColor);
-   })
-
-   function playSound(name){
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
-   }
-
-   function animatePress(currentColor){
-    
-   }
-
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
 }
 
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
 
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
 
 nextSequence();
 
